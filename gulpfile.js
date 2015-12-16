285578a76411275a7d4bf2537aa2dec1ffc71a2f
Daniel McLaughlin <danielsmc@gmail.com>
2015-12-16 08:30:14 -0500

    just can't leave the gulpfile alone

--- gulpfile.js
+++ gulpfile.js
@@ -29,59 +29,57 @@ gulp.task('gae-serve', function () {
     .pipe(gae('dev_appserver.py', [], {}));
 });
 
-var pipeline_spec = function() {
-  return {
-    backend: {
-      src: "src/backend/**/*",
-      dest: "dist",
-      munges: []
-    },
-    static: {
-      src: "src/static/**/*",
-      dest: "dist/static",
-      munges: []
-    },
-    templates: {
-      src: "src/templates/**/*",
-      dest: "dist/templates",
-      munges: []
-    },
-    images: {
-      src: "src/images/**/*",
-      dest: "dist/static/images",
-      munges: [
-                [cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))]
-              ]
-    },
-    styles: {
-      src: "src/styles/**/*.scss",
-      dest: "dist/static/styles",
-      munges: [
-                [sass(),
-                 autoprefixer('last 2 versions')],
-                [rename({suffix: '.min'}),
-                 minifycss()]
-              ]
-    },
-    scripts: {
-      src: "src/scripts/**/*.js",
-      dest: "dist/static/scripts",
-      munges: [
-                [jshint(),
-                 jshint.reporter('default'),
-                 concat('main.js')],
-                [rename({suffix: '.min'}),
-                 uglify()]
-              ]
-    },
-  };
+var pipelines = {
+  backend: {
+    src: "src/backend/**/*",
+    dest: "dist",
+    munges: []
+  },
+  static: {
+    src: "src/static/**/*",
+    dest: "dist/static",
+    munges: []
+  },
+  templates: {
+    src: "src/templates/**/*",
+    dest: "dist/templates",
+    munges: []
+  },
+  images: {
+    src: "src/images/**/*",
+    dest: "dist/static/images",
+    munges: [
+              ["cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))"]
+            ]
+  },
+  styles: {
+    src: "src/styles/**/*.scss",
+    dest: "dist/static/styles",
+    munges: [
+              ["sass()",
+               "autoprefixer('last 2 versions')"],
+              ["rename({suffix: '.min'})",
+               "minifycss()"]
+            ]
+  },
+  scripts: {
+    src: "src/scripts/**/*.js",
+    dest: "dist/static/scripts",
+    munges: [
+              ["jshint()",
+               "jshint.reporter('default')",
+               "concat('main.js')"],
+              ["rename({suffix: '.min'})",
+               "uglify()"]
+            ]
+  },
 };
 
-var pipeline_names = Object.keys(pipeline_spec());
+var pipeline_names = Object.keys(pipelines);
 
 pipeline_names.forEach(function(pn) {
   gulp.task(pn, function() {
-    var p = pipeline_spec()[pn];
+    var p = pipelines[pn];
     var g = gulp.src(p.src)
                 .pipe(plumber({
                   errorHandler: function (error) {
@@ -91,7 +89,7 @@ pipeline_names.forEach(function(pn) {
     if (p.munges.length) {
       p.munges.forEach(function(m) {
         m.forEach(function(step) {
-          g = g.pipe(step);
+          g = g.pipe(eval(step));
         });
         g = g.pipe(gulp.dest(p.dest));
       });
@@ -105,7 +103,6 @@ pipeline_names.forEach(function(pn) {
 gulp.task('pipelines',pipeline_names);
 
 gulp.task('watch-pipelines',function() {
-  var pipelines = pipeline_spec();
   pipeline_names.forEach(function(pn) {
     gulp.watch(pipelines[pn].src,[pn]);
   });
