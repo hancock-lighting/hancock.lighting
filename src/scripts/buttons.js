9fac4563a390c44361325ee3b2c5812a292958c8
Daniel McLaughlin <danielsmc@gmail.com>
2016-08-21 18:24:53 -0400

    Change the beacon by clicking the poem

---
+++ src/scripts/buttons.js
@@ -0,0 +1,14 @@
+function buttonInit() {
+  var buttons = document.querySelectorAll("button.c-poem__condition-switch");
+  Array.prototype.filter.call(buttons,function(b) {
+    b.addEventListener("click",function buttonClick() {
+      document.body.className = "is-"+this.dataset.beacon+" is-"+window.wb_status.time
+    });
+  });
+}
+
+if (document.readystate == "loading") {
+  document.addEventListener("DOMContentLoaded", buttonInit);
+} else {
+  buttonInit();
+}
